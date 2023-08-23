465ac977a2860505ddd540c82ee868b266d64400
Dexy98 <davidecandia@live.it>
2023-08-23 20:03:17 +0200

    primo inizio sviluppo app. si inizia

--- src/App.js
+++ src/App.js
@@ -1,10 +1,27 @@
+import React from "react";
 import './App.css';
+import { BrowserRouter as Router, Routes, Route } from 'react-router-dom'
+import Header from "./Componenti/Header";
+import Footer from "./Componenti/Footer";
+import Home from "./Componenti/Home";
 
 function App() {
   return (
-    <div className="">
-      <h1>HOME</h1>
+    <Router>
+    <div className=' max-w-5xl bg-slate-500 w-full h-auto mx-auto'>
+      <h1 className=" text-red-500">ciao</h1>
+      <Header />
+
+      <Routes>
+
+        <Route path='/movies' exact element={<Home />}></Route>
+        {/* <Route path='/movies/Storia' element={<Storia />}></Route> */}
+        {/* <Route path='/movies/Shop' element={<Shop />}></Route> */}
+
+      </Routes>
+      <Footer />
     </div>
+  </Router>
   );
 }
 
